f18d81b79c4b315d43f66f7bd8240df7eb4ea7a7
Ryan Prichard <ryan.prichard@gmail.com>
2012-09-27 22:29:26 -0700

    Remove a comment.  The rationale for Process is recorded in the commit log.
    Perhaps with more Qt experience, the API would seem more reasonable.
    Maybe.
    
    Writing the fork/exec code myself gives me control over the
    file-description-leakage problem that multi-threaded programs tend to have.
    (e.g. I could add a post-fork loop to close FDs if I needed.)  So far, I
    suspect that some FDs are leaking, but that it doesn't break anything.

--- clang-indexer/Process.h
+++ clang-indexer/Process.h
@@ -15,19 +15,6 @@ class ProcessPrivate;
 // handles instead of just one.  Both of them do blocking I/O, so it's
 // necessary to alternate between parent->child and child->parent
 // communication, but that's fine for the daemon.
-//
-// I decided to create this class instead of using QProcess.  QProcess's API
-// was confusing to me.  For example, does waitForReadyReady unblock if the
-// process dies?  It's not "ready-to-read", but the input pipe should have been
-// closed.  Closing the input pipe will unblock a native read/ReadFile call.
-// How do waitForReadyRead and readLine interact?  I would prefer to have a
-// readLine call that blocks until it reads one line, but does
-// QProcess::readLine block?  It looks like it doesn't?  What if there's a
-// partial line in the input?  Will readLine return as much of the line as it
-// can?  I don't want that, but what if it doesn't return anything?  It seems
-// that waitForReadyRead never blocks if there are bytes available to read,
-// but I would have guessed the opposite from the documentation.
-//
 class Process
 {
 public:
