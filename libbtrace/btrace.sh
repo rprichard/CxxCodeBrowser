8fbf4010899fc0d031281bd349aa32ab9304c090
Ryan Prichard <ryan.prichard@gmail.com>
2012-12-15 17:45:21 -0800

    Make btrace.sh play better with other LD_PRELOAD hooks.
     * In particular, when btrace.sh is invoked as the child of fakeroot, it
       now adds itself to the front of LD_PRELOAD rather than replacing
       LD_PRELOAD outright (which would remove fakeroot's LD_PRELOAD hook).

--- libbtrace/btrace.sh
+++ libbtrace/btrace.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 TOP=$(cd $(dirname $0) && pwd)
-export LD_PRELOAD=$TOP/libbtrace.so
+
+# This code copies fakeroot's behavior by adding the library to the front of the
+# space-delimited LD_PRELOAD variable.
+#
+# TODO: It should follow fakeroot more closely by setting LD_LIBRARY_PATH to
+# include both a 32-bit and (on appropriate hosts) a 64-bit library directory.
+# It could then provide just the library basename to LD_PRELOAD, and the
+# dynamic linker would pick an architecture-appropriate library, instead of
+# warning about an incompatible library, as it currently does.
+
+LIB=$TOP/libbtrace.so
+SEP=""
+if [ -n "$LD_PRELOAD" ]; then
+    SEP=" "
+fi
+export LD_PRELOAD="$LIB$SEP$LD_PRELOAD"
+
 export BTRACE_LOG=$PWD/btrace.log
+
 "$@"
