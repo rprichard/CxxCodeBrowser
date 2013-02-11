d2f619a8282f83b3b20ff94ae0818c6ef8004af4
Ryan Prichard <ryan.prichard@gmail.com>
2013-02-11 02:49:19 -0800

    MurmurHash3: Fix a build error on FreeBSD 9.1 with g++47.
     * On FreeBSD 9.1 with the g++47 compiler, the compiler complains that a
       call to an always_inline function in fPIC mode, with -O2, is not inlined
       because it can be overwritten at link-time.
    
     * I am skeptical of the continued use of always_inline.  I think it could
       be removed, but I don't see the point in doing so.  If upstream ever
       fixes the build error, they should consider using "static inline" and
       nothing else.

--- third_party/libMurmurHash3/MurmurHash3.cpp
+++ third_party/libMurmurHash3/MurmurHash3.cpp
@@ -16,7 +16,7 @@
 
 #if defined(_MSC_VER)
 
-#define FORCE_INLINE	__forceinline
+#define FORCE_STATIC_INLINE	static __forceinline
 
 #include <stdlib.h>
 
@@ -29,14 +29,14 @@
 
 #else	// defined(_MSC_VER)
 
-#define	FORCE_INLINE __attribute__((always_inline))
+#define	FORCE_STATIC_INLINE __attribute__((always_inline)) static inline
 
-inline uint32_t rotl32 ( uint32_t x, int8_t r )
+static inline uint32_t rotl32 ( uint32_t x, int8_t r )
 {
   return (x << r) | (x >> (32 - r));
 }
 
-inline uint64_t rotl64 ( uint64_t x, int8_t r )
+static inline uint64_t rotl64 ( uint64_t x, int8_t r )
 {
   return (x << r) | (x >> (64 - r));
 }
@@ -52,12 +52,12 @@ inline uint64_t rotl64 ( uint64_t x, int8_t r )
 // Block read - if your platform needs to do endian-swapping or can only
 // handle aligned reads, do the conversion here
 
-FORCE_INLINE uint32_t getblock ( const uint32_t * p, int i )
+FORCE_STATIC_INLINE uint32_t getblock ( const uint32_t * p, int i )
 {
   return p[i];
 }
 
-FORCE_INLINE uint64_t getblock ( const uint64_t * p, int i )
+FORCE_STATIC_INLINE uint64_t getblock ( const uint64_t * p, int i )
 {
   return p[i];
 }
@@ -65,7 +65,7 @@ FORCE_INLINE uint64_t getblock ( const uint64_t * p, int i )
 //-----------------------------------------------------------------------------
 // Finalization mix - force all bits of a hash block to avalanche
 
-FORCE_INLINE uint32_t fmix ( uint32_t h )
+FORCE_STATIC_INLINE uint32_t fmix ( uint32_t h )
 {
   h ^= h >> 16;
   h *= 0x85ebca6b;
@@ -78,7 +78,7 @@ FORCE_INLINE uint32_t fmix ( uint32_t h )
 
 //----------
 
-FORCE_INLINE uint64_t fmix ( uint64_t k )
+FORCE_STATIC_INLINE uint64_t fmix ( uint64_t k )
 {
   k ^= k >> 33;
   k *= BIG_CONSTANT(0xff51afd7ed558ccd);
